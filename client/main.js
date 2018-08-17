9735d04b7d9886f77c3fc63daa6e5c303def5b5e
Ostap Yaremchuk <ostiky@gmail.com>
2018-08-17 16:55:24 +0300

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,17 @@
+import './main.html';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+import {Players, calculatePlayerPositions} from './../imports/api/players';
+import App from './../imports/ui/App';
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players = Players.find({}, {sort: {score: -1}}).fetch();
+    let positionedPlayer = calculatePlayerPositions(players);
+    let title = 'Score keep';
+    ReactDOM.render(<App players={positionedPlayer} title={title} name={name}/>, document.getElementById('app'));
+  });
+});
